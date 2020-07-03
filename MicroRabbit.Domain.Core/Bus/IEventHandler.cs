7fad2fa17244e133f11cda4d2e73a3522643ca3d
Costin Somodea <costin@teamcode.ro>
2020-07-03 16:15:49 +0300

    partially completed banking microservice

---
+++ MicroRabbit.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,21 @@
+﻿using MicroRabbit.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    //IEventHandler takes in any type of event
+    public interface IEventHandler<in TEvent>: IEventHandler 
+        where TEvent: Event
+    {
+        Task Handle(TEvent @event);
+    }
+
+    public interface IEventHandler
+    {
+
+    }
+
+}
